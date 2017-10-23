409e1244f1bd9e92b1ee8d6e35ea0e9337fbb829
dsoraluz <d.soraluz@gmail.com>
2017-10-23 11:30:28 -0400

    added title to website

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Official Website for Jonathan Sterritt';
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
